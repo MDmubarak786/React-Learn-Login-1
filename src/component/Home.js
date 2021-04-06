6e20d6540e40d61cc72c6484fa08ba4dfbe8c8d9
MDmubarak786 <mohammedmubarakmk@gmail.com>
2021-04-06 18:52:50 +0530

    login success

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Switch, Route, Link } from "react-router-dom";
+import {  Link } from "react-router-dom";
 export default class Home extends Component {
     render() {
         return (
--- src/component/Login.js
+++ src/component/Login.js
@@ -2,19 +2,50 @@ import React, { Component } from 'react';
 
 
 class LoginPage extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    // disabled = () => {
+    //     return this.state.email.length > 0 && this.state.password.length > 0;
+    // }
+
+    handle = (e) => {
+        e.preventDefault();
+        console.log(this.state.email);
+        console.log(this.state.password);
+        this.setState({
+            email: '',
+            password: ''
+        });
+    }
     render() {
         return (
             <div>
                 <h1>Login Page</h1>
-                <form>
-                    <div>
+                <form onSubmit={this.handle}>
+                    <div className="Email">
                         <label htmlFor="email">Email :</label>
-                        <input type="email" placeholder="Enter your email"  required autoFocus/>
+                        <input
+                            type="email"
+                            placeholder="Enter your email"
+                            value={this.state.email}
+                            onChange={(e) => this.setState({ email: e.target.value })}
+                            required
+                            autoFocus />
                     </div>
-                    <div>
+                    <div className="Password">
                         <label htmlFor="password">Password :</label>
-                        <input type="password" placeholder="Enter password"  required />
+                        <input
+                            type="password"
+                            placeholder="Enter password"
+                            value={this.state.password}
+                            onChange={(e) => this.setState({ password: e.target.value })}
+                            required
+                        />
                     </div>
+                    {/* disabled={!this.disabled()} => disabled function using this you can't click the submit button */}
                     <button>Submit</button>
                     <p>Already have an account <a href="/Signin">Sign in</a></p>
 
