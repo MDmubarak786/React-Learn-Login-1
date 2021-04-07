1f5cfbed149ae7c29f636fbacd6a3cc427a9f152
MDmubarak786 <mohammedmubarakmk@gmail.com>
2021-04-07 08:45:38 +0530

    left side created

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import '../App.css'
 
 class LoginPage extends Component {
     state = {
@@ -22,36 +22,45 @@ class LoginPage extends Component {
     }
     render() {
         return (
-            <div>
-                <h1>Login Page</h1>
-                <form onSubmit={this.handle}>
-                    <div className="Email">
-                        <label htmlFor="email">Email :</label>
-                        <input
-                            type="email"
-                            placeholder="Enter your email"
-                            value={this.state.email}
-                            onChange={(e) => this.setState({ email: e.target.value })}
-                            required
-                            autoFocus />
+            <div className="split">
+                <div className="split left login">
+                  
+                    <form onSubmit={this.handle}>
+                        <div className="Email">
+                            <h1>Login Page</h1>
+                            <h3>Welcome Back</h3>
+                            {/* <h3>Email :</h3> */}
+                            <input
+                                id='email'
+                                type="email"
+                                placeholder="Enter your email"
+                                value={this.state.email}
+                                onChange={(e) => this.setState({ email: e.target.value })}
+                                required
+                                autoFocus />
+                        </div>
+                        <div className="Password">
+                            {/* <h3>Password :</h3> */}
+                            <input
+                                id='password'
+                                type="password"
+                                placeholder="Enter password"
+                                value={this.state.password}
+                                onChange={(e) => this.setState({ password: e.target.value })}
+                                required
+                            />
+                        </div>
+                        {/* disabled={!this.disabled()} => disabled function using this you can't click the submit button */}
+                        <button className="btn">Submit</button>
+                        <p>Already have an account <a href="/Signin" className="signin-link-text">Sign in</a></p>
+                    </form>
+
+                </div>
+                <div className="split right">
+                    <div className="blur">
+                        <h1>Welcome to Login Page</h1>
                     </div>
-                    <div className="Password">
-                        <label htmlFor="password">Password :</label>
-                        <input
-                            type="password"
-                            placeholder="Enter password"
-                            value={this.state.password}
-                            onChange={(e) => this.setState({ password: e.target.value })}
-                            required
-                        />
-                    </div>
-                    {/* disabled={!this.disabled()} => disabled function using this you can't click the submit button */}
-                    <button>Submit</button>
-                    <p>Already have an account <a href="/Signin">Sign in</a></p>
-
-
-
-                </form>
+                </div>
             </div>
         )
     }
